ea230ba6387ab0926ce232b21ca34bfd1b211145
Наталья Ярцева <zabaluna@mail.ru>
2022-12-04 00:14:44 +0300

    FindThreeStringElements

--- StudentTask/Program.cs
+++ StudentTask/Program.cs
@@ -1,14 +1,27 @@
-﻿
+﻿string[] firstarray = new string[6] { "0", "two", "three", "four", "five", "12345" };
+string[] resultarray = new string[firstarray.Length];
+
+void FindThreeStringElements(string[] firstarray, string[] resultarray)
+{
+    int count = 0;
+    for (int i = 0; i < firstarray.Length; i++)
+    {
+        if (firstarray[i].Length <= 3)
+        {
+            resultarray[count] = firstarray[i];
+            count++;
+        }
+    }
+}
+
 void PrintArray(string[] array)
 {
     for (int i = 0; i < array.Length; i++)
     {
-        Console.Write($"{array[i]}");
+        Console.Write($" {array[i]}");
     }
-    Console.WriteLine();
+    Console.Write("-> Cтроки длина которых меньше или равна 3 символам:");
 }
-
-
 
 
 
